ffeca2276fa9a1dafa3090f30d250ad69fb59422
Achmad Romli <aliansi.corp@gmail.com>
2023-10-01 09:58:09 +0700

    First commit

--- cmd/main.go
+++ cmd/main.go
@@ -8,30 +8,22 @@ import (
 	"syscall"
 
 	"double-dose/discovery/internal/discovery"
-	"double-dose/discovery/internal/service"
 )
 
 func main() {
-	// Buat service discovery
 	sd, err := discovery.NewServiceDiscovery()
 	if err != nil {
 		log.Fatalf("Failed to create service discovery: %v", err)
 	}
-	defer sd.DeregisterService("my-service")
+	defer sd.DeregisterService("order-management")
 
-	// Daftarkan layanan ke service discovery
-	serviceID := "my-service"
+	serviceID := "order-management"
 	servicePort := 8080
-	err = sd.RegisterService("my-service", serviceID, "localhost", servicePort)
+	err = sd.RegisterService("order-management", serviceID, "localhost", servicePort)
 	if err != nil {
 		log.Fatalf("Failed to register service: %v", err)
 	}
 
-	// Inisialisasi dan jalankan layanan
-	myService := service.NewMyService(servicePort)
-	go myService.Start()
-
-	// Tunggu sinyal SIGINT atau SIGTERM untuk menghentikan layanan
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 	<-sigCh
